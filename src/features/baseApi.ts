5d1b0c557017d0221fff52a4ebf91b7e76fd2d3a
Haneul Jin <hnjin@citus.co.kr>
2026-01-02 14:51:06 +0900

    Refactor base API URL configuration to dynamically set base URL based on environment, supporting both production and development environments.

--- src/features/baseApi.ts
+++ src/features/baseApi.ts
@@ -1,7 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
-// const baseUrl = "https://fire-api.haneul.app"
-const baseUrl = "http://localhost:3000"
+const baseUrl =
+  import.meta.env.NODE_ENV === "production"
+    ? "https://fire-api.haneul.app"
+    : "http://localhost:3000"
+
 const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({ baseUrl }),
